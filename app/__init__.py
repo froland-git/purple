4d2377cdd82a0faa33b2d966bcf6a8bb982bf5f7
Andrey Frolenkov <54927103+froland-git@users.noreply.github.com>
2022-04-29 18:07:20 +0300

    [07a] Lagre project structure (#20)
    * [07a] Lagre project structure
    
    * add nl
    
    * add nl
    
    * Cosmetic
    
    * After test
    
    * Changes

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+from config import config # from sys.path/config.py import dictionary config
+
+
+bootstrap = Bootstrap()
+moment = Moment()
+db = SQLAlchemy()
+mail = Mail()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app) # init_app is not a staticmethod from config.py
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    from .main import main as main_blueprint # from ./main
+    app.register_blueprint(main_blueprint)
+
+    return app
---
+++ config.py
@@ -0,0 +1,52 @@
+import os.path
+
+basedir = os.path.abspath(os.path.dirname(__file__)) # SQLite - sqlite:///absolute/path/to/database
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+        ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    PURPLE_MAIL_SUBJECT_PREFIX = '[Purple]'
+    PURPLE_MAIL_SENDER = 'Purple Admin <purple@example.com>'
+    PURPLE_ADMIN = os.environ.get('PURPLE_ADMIN')
+
+    # static method is easier to import than regular functions because each function does not need to be separately imported
+    # Myclass.staticmethod()
+    # or
+    # my_obj = Myclass()
+    # my_obj.staticmethod()
+    @staticmethod
+    def init_app(app):
+        pass # this is plug
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-prod.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
