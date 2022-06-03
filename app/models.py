318fbbaf82d867a302112db9d2e567e7ba712c9b
Frolenkov Andrey <afrolenkov@wiley.com>
2022-06-03 15:54:01 +0300

    [08e] itsdangerous: account confirmation

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import db, login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer # for web-signature JSON
+from flask import current_app
 
 
 class Role(db.Model):
@@ -20,6 +22,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128)) # This is like __init__ in ordinary class
+    confirmed = db.Column(db.Boolean, default=False)
 
     # Doc: https://www.tutorialsteacher.com/python/property-decorator
     @property
@@ -32,6 +35,26 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    # Generate marker
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        # Method dumps() generates digital signature and create marker
+        return s.dumps({'confirm': self.id}) # 'confirm' default key, self.id - value
+
+    # Check marker and save it in confirmed
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            # Method loads() decrypts marker
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self) # db.session.add() for prepare, db.session.commit() for for the record
+        return True
 
     def __repr__(self):
         return '<User %r>' % self.username
