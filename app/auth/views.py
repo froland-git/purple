e180a24c583b8c6de2d9e1cc372e2b9b001ced9b
Andrey Frolenkov <54927103+froland-git@users.noreply.github.com>
2022-06-23 17:59:15 +0300

    [08e] itsdangerous: account confirmation (#25)
    * [08e] itsdangerous: account confirmation
    
    * Add logic
    
    * Add logic for email confirmation
    
    * Add logic for unconfirmed users
    
    * Add unit tests
    
    * Add nl
    
    * Add resend email function
    
    * New records in DB

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,6 +6,28 @@ from .forms import LoginForm, RegistrationForm
 from .. import db
 from ..email import send_email
 
+#----- [08e] Actions for unconfirmed users START-----
+# use decorator before_app_request()
+@auth.before_app_request
+def before_request():
+    # is_authenticated - https://flask-login.readthedocs.io/en/latest/
+    #     # current_user.confirmed - from model.py
+    #     # request.endpoint - https://stackoverflow.com/questions/19261833/what-is-an-endpoint-in-flask
+    #     # request.endpoint should be out of blueprint 'auth'
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.endpoint \
+            and request.blueprint != 'auth' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+#----- [08e] Actions for unconfirmed users END-----
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -35,7 +57,34 @@ def register():
             password=form.password.data
         )
         db.session.add(user)
-        flash('You can now login.')
-        return redirect(url_for('auth.login'))
+        #-----[08e] Block for sending email confirmation-----
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm Your Account',
+                   'auth/email/confirm', user=user, token=token)
+        flash('A confirmation email has been sent to you by email.')
+        return redirect(url_for('main.index'))
+        #-----[08e] End block-----
     return render_template('auth/register.html', form=form)
 
+#----- [08e] Function to confirm account -----
+@auth.route('/confirm/<token>')
+@login_required # Decorator form Flask-Login. Need to login on a first step
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
+
+#----- [08e] Function to resend confirmation email -----
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account',
+               'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
