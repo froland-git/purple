bbe02124aeb039bc9ae05f590e38717039aa1f46
Frolenkov Andrey <afrolenkov@wiley.com>
2022-06-23 16:28:41 +0300

    Add unit tests

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,15 +1,18 @@
 import unittest
 from app import create_app, db
 from app.models import User
+import time
 
 
 class UserModelTestCase(unittest.TestCase):
+    # Basic method performed before all test_<name> methods
     def setUp(self):
         self.app = create_app('testing')
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
 
+    # Basic method performed after all test_<name> methods
     def tearDown(self):
         db.session.remove()
         db.drop_all()
@@ -33,3 +36,30 @@ class UserModelTestCase(unittest.TestCase):
         u = User(password='cat')
         u2 = User(password='cat')
         self.assertTrue(u.password_hash != u2.password_hash)
+
+    # ----- [08e] Generate and check confirm token -----
+    def test_valid_confirmation_token(self):
+        u = User(password='cat')
+        db.session.add(u) # prepare to add
+        db.session.commit() # app data to db
+        token = u.generate_confirmation_token() # Generate marker
+        self.assertTrue(u.confirm(token)) # Check marker and save it in confirmed field in Model
+
+    # ----- [08e] Use invalid configmation token for existing user -----
+    def test_invalid_confirmation_token(self):
+        u1 = User(password='cat')
+        u2 = User(password='dog')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        token = u1.generate_confirmation_token()
+        self.assertFalse(u2.confirm(token)) # False if we use wrong token for user
+
+    # ----- [08e] Check expiration -----
+    def test_expired_confirmation_token(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token(1) # expiration = 1 sec
+        time.sleep(2)
+        self.assertFalse(u.confirm(token))
