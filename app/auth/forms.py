7ffddaff84863a50eb1cbc843ba73fd3cade3ef8
Frolenkov Andrey <afrolenkov@wiley.com>
2022-07-01 14:27:01 +0300

    Fix issues

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -44,6 +44,7 @@ class PasswordResetRequestForm(FlaskForm):
     submit = SubmitField('Reset Password')
 
 class PasswordResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
     password = PasswordField('New Password', validators=[DataRequired(), EqualTo('password2', message='Passwords must match')])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
     submit = SubmitField('Reset Password')
