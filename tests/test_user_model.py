426ca053be5ce9c78b922fbcc1ea4352ec2877c6
Frolenkov Andrey <afrolenkov@wiley.com>
2022-07-05 18:28:10 +0300

    fix bugs

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -75,14 +75,14 @@ class UserModelTestCase(unittest.TestCase):
 
     # ----- [08g] Test how we can reset token (false) -----
     def test_invalid_reset_token(self):
-        u1 = User(password='cat')
+        u = User(password='cat')
         u2 = User(password='dog')
-        db.session.add(u1)
+        db.session.add(u)
         db.session.add(u2)
         db.session.commit()
-        token = u1.generate_reset_token()
-        self.assertFalse(u2.reset_password(token, 'horse'))
-        self.assertTrue(u2.verify_password('dog'))
+        token = u.generate_reset_token()
+        self.assertFalse(u.reset_password(token + 'a', 'horse'))
+        self.assertTrue(u.verify_password('cat'))
 
     # ----- [08g] Test how we can change email address (valid) -----
     def test_valid_email_change_token(self):
