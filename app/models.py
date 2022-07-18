c032490d2a34d91f13ded6102b4743c9d3b09ede
Frolenkov Andrey <afrolenkov@wiley.com>
2022-07-18 13:05:25 +0300

    [10a] User Profile

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer # for web-signature JSON
 from flask import current_app
+from datetime import datetime
 
 
 # ----- [09a] Permission bitmask class -----
@@ -63,14 +64,20 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
+# https://docs.sqlalchemy.org/en/14/core/type_basics.html
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    password_hash = db.Column(db.String(128)) # This is like __init__ in ordinary class
+    password_hash = db.Column(db.String(128))  # This is like __init__ in ordinary class
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)  # datetime.utcnow is a function (without ()) in db.Column()
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
 
     # ----- [09a] Set role 'Administrator' or 'Default' role  -----
     def __init__(self, **kwargs):
@@ -173,6 +180,11 @@ class User(UserMixin, db.Model):
 
     def is_administrator(self):
         return self.can(Permission.ADMINISTER)  # Invoke method can
+
+    # ----- [10a] Method to update field 'last_seen' -----
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
 
     def __repr__(self):
         return '<User %r>' % self.username
